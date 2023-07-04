96f6c4ac34229cf04103d055db56e540c7941f62
Ghaith-Abuhussain <e.hamadahtrraf@gmail.com>
2023-07-04 18:23:15 +0300

    first commit

---
+++ src/main/java/com/example/demo_postger/DemoPostgerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_postger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPostgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPostgerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo_postger/DemoPostgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_postger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPostgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
